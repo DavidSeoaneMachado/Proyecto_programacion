989f79ee7b089fd0ddd75ef9259236c60508b54b
DavidSeoaneMachado <dseoanemachado@danielcastelao.org>
2023-05-04 12:52:30 +0200

    Commit desde David con Perfil_cliente DAO

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -1,9 +1,12 @@
 package Controller;
-
+import View.Menu_principal;
 
 public class Controller {
     public static void main(String[] args) {
 
 
+       // Inicio.generar_ventana_inicio();
+        Menu_principal menuPrincipal = new Menu_principal();
+        menuPrincipal.sacarventana();
     }
 }
