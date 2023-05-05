eebd815c7aaf78039828dd037b72894068120e31
DavidSeoaneMachado <dseoanemachado@danielcastelao.org>
2023-05-05 19:06:46 +0200

    Interfaces gráficas de Inicio y Menu_principal listas (sin funcionalidades)

--- src/View/Inicio.java
+++ src/View/Inicio.java
@@ -1,79 +1,38 @@
 package View;
 
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Inicio {
+    private JButton crearPerfilButton;
+    private JButton iniciarSesiónButton;
+    private JPanel Inicio;
+    private JPanel Header;
+    private JPanel Body;
+    private JPanel Footer;
+    private JButton salirButton;
 
-    public static void generar_ventana_inicio(){
-
-        //Creamos el la ventana//
-        JFrame ventana = new JFrame();
-        ventana.setBounds(0, 0, 1200, 800);
-        ventana.setTitle("Fitness | Inicio");
-        ventana.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-
-        //Creamos el layout y se los asignamos a la ventana//
-        GridLayout grid = new GridLayout(3, 0);
-        grid.setHgap(30);
-        grid.setVgap(10);
-        ventana.setLayout(grid);
-
-        //Primer elemento de la venatana Inicio//
-        JPanel header = new JPanel();
-        header.setLayout(new GridLayout(5, 0));
-        //Etiqueta superior//
-        JLabel etiqueta = new JLabel();
-        etiqueta.setText("...Fitness");
-        JLabel etiqueta2 = new JLabel();
-        etiqueta2.setText("¿Quieres alcanzar tu mejor estado fisico y crear habitos saludables? ...Fitness es lo que estabas buscando.");
-        header.add(etiqueta);
-        header.add(etiqueta2);
-        etiqueta.setHorizontalAlignment(JLabel.CENTER);
-        etiqueta2.setHorizontalAlignment(JLabel.CENTER);
-
-        //Segundo elemento de la venatana Inicio//
-        JPanel body = new JPanel();
-        body.setLayout(new FlowLayout());
-        //Boton 1//
-        JButton boton = new JButton();
-        boton.setLabel("Crear perfil/cuenta");
-        boton.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
-        //Boton 2//
-        JButton boton2 = new JButton();
-        boton2.setLabel("Iniciar sesión");
-        boton2.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
-
-        body.add(boton);
-        body.add(boton2);
-
-        //Tercer elemento de la ventana Inicio//
-        JPanel footer = new JPanel();
-        footer.setLayout(new FlowLayout());
-        //Boton//
-        JButton boton_salir = new JButton();
-        boton_salir.setLabel("Salir");
-        boton_salir.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
-        //Funcionalidad: salir//
-        boton_salir.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.exit(0);
-            }
-        });
-        footer.add(boton_salir);
-
-
-
-        //Añadimos los elementos a la ventana Inicio//
-        ventana.add(header);
-        ventana.add(body);
-        ventana.add(footer);
-
-        //Hacemos visble la ventana//
-        ventana.setVisible(true);
+    /**
+     * Constructor del entorno gráfico del inicio de la aplicacion
+     * llama al metodo que tendrá los listener de esta interfaz gráfica
+     */
+    public Inicio() {
+        activar_listeners();
     }
 
+    public void lanzar_ventana() {
+
+        JFrame ventana = new JFrame("Verdanks Fiteness");
+        ventana.setContentPane(Inicio);
+        ventana.setBounds(0, 0, 1200, 800);
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ventana.setVisible(true);
+
+    }
+
+    private void activar_listeners() {
+
+
+    }
 }
--- src/View/Menu_principal.java
+++ src/View/Menu_principal.java
@@ -4,21 +4,37 @@ import javax.swing.*;
 
 public class Menu_principal {
     private JPanel panel1;
+    private JPanel Header;
+    private JButton nuevaDietaButton;
+    private JButton nuevoEntrenamientoButton;
+    private JButton verPlanningActualButton;
+    private JButton descargarPlanningButton;
+    private JPanel Body;
+    private JPanel Footer;
+    private JButton editarPerfilButton;
+    private JButton salirButton;
 
-    private JPanel header;
-    private JPanel body;
-    private JPanel footer;
-    private JButton button1;
+    /**
+     * Constructor del entorno gráfico del menu principal
+     * llama al metodo que tendrá los listener de esta interfaz gráfica
+     */
+    public Menu_principal() {
 
-    public void sacarventana(){
+        activar_listeners();
 
-        JFrame frame = new JFrame();
-        frame.setContentPane(panel1);
-        frame.setBounds(0, 0, 1200, 800);
-        frame.setVisible(true);
     }
 
-    private void createUIComponents() {
-        // TODO: place custom component creation code here
+    public void lanzar_ventana(){
+
+        JFrame ventana = new JFrame("Verdanks Fiteness");
+        ventana.setContentPane(new Menu_principal().panel1);
+        ventana.setBounds(0, 0, 1200, 800);
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ventana.setVisible(true);
+    }
+
+    private void activar_listeners() {
+
+
     }
 }
