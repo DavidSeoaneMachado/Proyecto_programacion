7eccc5344510d27c43f8e3abe26b0689b08af612
fernando-daw <114103916+fernando-daw@users.noreply.github.com>
2023-05-23 16:16:53 +0200

    Fer (#14)
    * Creacion de la clase Ejercicios con los arrays de los ejercicios
    
    * Introduccion de metodos a realizar para completar la clase dieta
    
    * resolucion de metodos y FORM de la dieta
    
    * introduccion de los arrays y  metodos de ejercicios y comidas para seleccionarse de forma aleatoria
    
    * arreglo de problemas en metodos de dieta
    
    * commit
    
    * forms y codigo de dieta

---
+++ src/Model/Comidas.java
@@ -0,0 +1,31 @@
+package Model;
+
+import java.util.Random;
+
+public class Comidas {
+    //Arrays con las comidas
+
+    String[] desayuno = new String[]{"Claras de huevo", "Avena", "Tostada integral con jamon serrano y tomate"};
+    String[] comida = new String[]{"Arroz con pollo", "Patatas con ternera", "Boniato con pavo"};
+    String[] cena = new String[]{"Merluza con ensalada", "Marisco", "Calamares a la plancha"};
+
+    public void Escoger_Comida() {
+        Random ale = new Random();
+        int desayunoAle = ale.nextInt(desayuno.length);
+        int comidaAle = ale.nextInt(comida.length);
+        int cenaAle = ale.nextInt(cena.length);
+
+        String primeracomida = desayuno[desayunoAle];
+        String segundacomida = comida[comidaAle];
+        String terceracomida = cena[cenaAle];
+
+        System.out.println("Primera comida del dia: " + primeracomida);
+        System.out.println("Primera comida del dia: " + segundacomida);
+        System.out.println("Primera comida del dia: " + terceracomida);
+    }
+
+    public static void main(String[] args) {
+        Comidas comidas = new Comidas();
+        comidas.Escoger_Comida();
+    }
+}
--- src/Model/Dieta.java
+++ src/Model/Dieta.java
@@ -2,16 +2,103 @@ package Model;
 
 public abstract class Dieta {
 
-    private int kcal_totales;
-    private int proteinas;
+    private double kcal_totales;
+    private double proteinas;
     private int hidratos;
+    private int grasas;
+    private int TMB;
+    private double Factor_Actividad_Poco = 1.2;
+    private double Factor_Actividad_Ligero = 1.375;
+    private double Factor_Actividad_Moderado = 1.550;
+    private double Factor_Actividad_Fuerte = 1.725;
+    private double Factor_Actividad_MuyFuerte = 1.9;
+    private String volumen;
+    private String definicion;
+    private String mantenimiento;
+
 
     /**
      * Metodo que genera un tipo de dieta en funcion de las caracteristicas del cliente
      * @param cliente ya registrado con determinadas caracteristicas y objetivos
      */
-    public void generar_dieta(Perfil_cliente cliente){
+    public int Generar_Tasa_Metabolica_Basal(Perfil_cliente cliente) {
 
-    };
+        int mujer = 1;
+        int hombre = 2;
+        int variable = 0;
 
+        switch (variable) {
+            if (cliente.sexo = mujer) {
+                TMB = 655 + (9.6 * cliente.getPeso() + (1.8 * cliente.getAltura() - (4.7 * cliente.getEdad())));
+            }
+            else (cliente.sexo = hombre) {
+                TMB = 66 + (13.7 * cliente.getPeso() + (5 * cliente.getAltura() - (6.8 * cliente.getEdad())));
+            }
+        } ;
+        return TMB;
+    }
+    /**
+     * Metodo que calcula los hidratos de carbono a consumir en funcion de las caracteristicas del cliente
+     * @param FactorActividad, peso ya registrado con determinadas caracteristicas y objetivos
+     */
+public int Calcular_Hidratos(int tasa,double peso,String FactorActividad){
+    //para calcular los hidratos de carbono, tenemos que tener en cuenta los requisitos del cliente ya que dependera si quire una definicion o encontrarse en una etapa de volumen
+tasa=TMB;
+double Gramos_Hidratos_Carbono;
+double factorActividadFisica=0;
+
+switch ( factorActividadFisica){
+    case "Poco Ejercicio":
+        factorActividadFisica=Factor_Actividad_Poco;
+        break;
+    case "Ejercicio Ligero":
+        factorActividadFisica=Factor_Actividad_Ligero;
+    case "Ejercicio Moderado":
+        factorActividadFisica=Factor_Actividad_Moderado;
+    case "Ejercicio Fuerte":
+        factorActividadFisica=Factor_Actividad_Fuerte;
+    case "Ejercicio Muy Fuerte":
+        factorActividadFisica=Factor_Actividad_MuyFuerte;
+        break;
+    default:
+        System.out.println("No has introducido un campo valido");
 }
+
+    return Gramos_Hidratos_Carbono;
+}
+
+    /**
+     * Metodo que calcula las proteinas a consumir en funcion de las caracteristicas del cliente
+     * @param cliente, etapa ya registrado con determinadas caracteristicas y objetivos
+     */
+    public double Calcular_Proteina(Perfil_cliente cliente,String etapa) {
+    int prote;
+        if (etapa == volumen) {
+            prote= (int) (cliente.getPeso() * 1.4);
+            return prote; //en volumen consideramos 1.2 gramos de proteina por Kg de peso
+
+        } else if (etapa == definicion) {
+            prote= (int) (cliente.getPeso() * 1.6);
+            return prote;//en volumen consideramos 1.6 gramos de proteina por Kg de peso
+        } else if (etapa == mantenimiento) {
+            prote= (int) (cliente.getPeso() * 1.1);
+            return prote; //en volumen consideramos 1.2 gramos de proteina por Kg de peso
+        }
+        return 0;
+    }
+    /**
+     * Metodo que calcula las grasas a consumir en funcion de las caracteristicas del cliente
+     * @param cliente, etapa ya registrado con determinadas caracteristicas y objetivos
+     */
+    public int Calcular_Grasas(Perfil_cliente cliente,String etapa){
+
+    }
+
+    public Mostrar_Datos (int hidrato, int prot, int fat) {
+    //recoger los resultados de los metodos anteiores y mostrarlos
+
+        System.out.println("Cliente deberia de consumir: x Gr de Hidratos de Carbono, X de Proteina y X de Grasas");
+    }
+
+
+
